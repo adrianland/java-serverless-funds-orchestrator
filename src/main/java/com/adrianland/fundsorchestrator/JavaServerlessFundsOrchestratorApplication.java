b5ea18d0df214328a5c9e8c4c12864b3fe733dab
adrian landazuri <aelandazurri@celsia.com>
2026-03-13 17:37:02 -0500

    Initial commit: Spring Boot project structure

---
+++ src/main/java/com/adrianland/fundsorchestrator/JavaServerlessFundsOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package com.adrianland.fundsorchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaServerlessFundsOrchestratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaServerlessFundsOrchestratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adrianland/fundsorchestrator/JavaServerlessFundsOrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrianland.fundsorchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaServerlessFundsOrchestratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
